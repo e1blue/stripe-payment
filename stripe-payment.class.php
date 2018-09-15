bd928cd2d7570d27cc8d0a2955d80dcd659c5421
taman777 <takeshi.satoh@gmail.com>
2018-09-16 00:03:04 +0900

    デバッグ削除

--- stripe-payment.class.php
+++ stripe-payment.class.php
@@ -333,11 +333,6 @@ function stripe_purchase() {
 		$finish_param = null;
 		$checkout_id  = $atts['checkout_id'];
 		$this->stripe_error_log( "================= stripe_payment_result_display =========:" . $checkout_id );
-		ob_start();
-		var_dump( $_REQUEST );
-		$var = ob_get_contents();
-		ob_end_clean();
-		$this->stripe_error_log( $var );
 		if ( isset( $_REQUEST['stripeToken'] ) &&
 		     ( empty( $_REQUEST['checkout_id'] ) ||
 		       $checkout_id === $_REQUEST['checkout_id'] ) ) {
